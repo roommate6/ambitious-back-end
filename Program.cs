0e246727a1c30d5859bf7ffe6710390550d4195c
roommate6 <floginstefan@gmail.com>
2024-12-19 19:52:21 +0200

    +cleaned Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-using caditec_back_end.Data.Models.Options.MediaService;
 using caditec_back_end.Services.Concretes;
-using caditec_back_end.Services.Interfaces;
 using caditec_back_end.Startups;
 using caditec_back_end.Startups.OfflineVerifiers;
 
@@ -15,20 +13,4 @@ builder.Services.AddEverythingCustomly(appSettingsService);
 
 var app = builder.Build();
 app.ConfigureEverythingCustomly();
-
-var formFile = new FormFile(new MemoryStream(System.Text.Encoding.UTF8.GetBytes("Test value aaa22")), 0, 16, "Data", "test.txt");
-var cts = new CancellationTokenSource();
-cts.CancelAfter(TimeSpan.FromSeconds(10));
-
-var createMediaOptions = new CreateMediaOptions
-{
-    FormFile = formFile,
-    DirectoryPath = app.Services.GetRequiredService<IAppSettingsService>().Configurations.Paths.MediaStorage,
-    CancellationToken = cts.Token
-};
-
-var result = await app.Services.GetRequiredService<IMediaService>().CreateAsync(createMediaOptions);
-Console.WriteLine(result.FilePath ?? result.ExceptionMessage);
-
-
 app.Run();
